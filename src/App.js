b6176554699e28bb5e7c5444243ceaddefb3de8d
MassimaMao <mastem@msn.com>
2022-06-27 22:11:18 +0200

    create context configuration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Contenu from './Components/Contenu/Contenu';
 import ToggleLangs from './Components/ToggleLangs/ToggleLangs';
+import ContextProvider from './context/LangContext';
 
 function App() {
   return (
-    <>
+    <ContextProvider>
       <ToggleLangs />
       <Contenu />
-    </>
+    </ContextProvider>
   );
 }
 
--- src/Components/Contenu/Contenu.js
+++ src/Components/Contenu/Contenu.js
@@ -1,12 +1,17 @@
 import { useContext } from 'react';
+import { Context } from '../../context/LangContext';
+
 import './Contenu.css';
 import data from '../../assets/data.js';
 
 export default function Contenu() {
+  const { lang } = useContext(Context);
+
+  console.log(lang);
   return (
     <div className='content'>
-      <h1 className='title'>{data['FR'].title}</h1>
-      <p className='content-txt'>{data['FR'].txt}</p>
+      <h1 className='title'>{data[lang].title}</h1>
+      <p className='content-txt'>{data[lang].txt}</p>
     </div>
   );
 }
